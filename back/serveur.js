be42aa6a1d64620e761153b8793ca178e7062254
Lilian PULVERIN <lilianpulv@gmail.com>
2026-05-31 17:42:12 +0200

    ajout route au serveur.js

--- back/serveur.js
+++ back/serveur.js
@@ -5,6 +5,7 @@ import cors from "cors";
 import path from "path";
 import { fileURLToPath } from "url";
 import authRoutes from "./routes/auth.js";
+import facturesRoutes from "./routes/facture.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname  = path.dirname(__filename);
@@ -17,6 +18,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, "../front")));
 
 app.use("/api/auth", authRoutes);
+app.use("/api/facture", facturesRoutes);
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`Serveur sur http://localhost:${PORT}`));
